78c1e34c38e6961c5d2d47318eb33e847e8bb01b
Mussorable <redduck5601@gmail.com>
2024-08-17 16:06:19 +0200

    added address section with folium map, ended about_us page

--- app/static/js/template_animations.js
+++ app/static/js/template_animations.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Make small images moving left-right with random speed in section around main image
     const images = document.querySelectorAll('.option-img');
 
     images.forEach(image => {
@@ -6,4 +7,21 @@ document.addEventListener('DOMContentLoaded', function() {
 
         image.style.animationDuration = `${randomDuration}s`;
     });
+
+    // Image shows up when scrolling down
+    const showsUpBlocks = document.querySelectorAll('.scrolling-fast-up');
+
+    function checkVisibility() {
+        showsUpBlocks.forEach(block => {
+            const rect = block.getBoundingClientRect();
+            const windowHeight = window.innerHeight;
+
+            if (rect.top <= windowHeight && rect.bottom >= 0) {
+                block.classList.add('visible');
+            }
+        })
+    }
+
+    window.addEventListener('scroll', checkVisibility);
+    window.addEventListener('resize', checkVisibility);
 })
