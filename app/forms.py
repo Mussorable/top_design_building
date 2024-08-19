d4861c62127eaf8de6d04f836bad3646e125ae28
Mussorable <redduck5601@gmail.com>
2024-08-19 22:28:33 +0200

    added styles for contact form, validation, invalid and in-focus classes, started making small add columns of information

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms.fields.simple import StringField, SubmitField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from wtforms.fields.simple import StringField, SubmitField, EmailField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, Regexp
 
 
 class ContactForm(FlaskForm):
     name = StringField('Your name', validators=[DataRequired()])
-    email = StringField('Your email', validators=[DataRequired(), Email()])
-    phone_number = StringField('Your phone number', validators=[DataRequired()])
-    message = StringField('Your message', validators=[DataRequired()])
+    email = EmailField('Your email', validators=[DataRequired(), Email()])
+    phone_number = StringField('Your phone number', validators=[
+        DataRequired(),
+        Regexp(r'^\+?1?\d{9,15}$', message="Invalid phone number format."),
+        Length(min=10, max=15, message="Phone number must be between 10 and 15 digits.")
+    ])
+    message = TextAreaField('Your message', validators=[DataRequired()])
     submit = SubmitField('Contact Us')
--- app/static/js/template_animations.js
+++ app/static/js/template_animations.js
@@ -24,4 +24,91 @@ document.addEventListener('DOMContentLoaded', function() {
 
     window.addEventListener('scroll', checkVisibility);
     window.addEventListener('resize', checkVisibility);
+
+    // Add .is-focused when input field is focused
+    const inputs = document.querySelectorAll('.form-control');
+    inputs.forEach(input => {
+        input.addEventListener('focus', function() {
+            this.parentNode.classList.add('is-focused');
+
+            const inputMessages = {
+              text: "We never share your sensitive personal information",
+              password: "PASSWORD MESSAGE",
+              email: "We will never share your email outside",
+            };
+
+            if (!this.parentNode.querySelector(".bmd-notation")) {
+                const span = document.createElement("span");
+                span.classList.add("bmd-notation");
+                span.textContent = inputMessages[this.type] || '';
+                this.parentNode.appendChild(span);
+            }
+        });
+
+        input.addEventListener('blur', function() {
+            this.parentNode.classList.remove('is-focused');
+
+            const span = this.parentNode.querySelector(".bmd-notation");
+            console.log(span);
+            if (span) {
+                this.parentNode.removeChild(span);
+            }
+
+            if (this.value !== "") {
+                this.parentNode.classList.add('is-filled');
+            } else {
+                this.parentNode.classList.remove('is-filled');
+            }
+        });
+
+        input.addEventListener("input", function() {
+            if (this.value !== "") {
+                this.parentNode.classList.add("is-filled");
+            } else {
+                this.parentNode.classList.remove("is-filled");
+            }
+        });
+
+        if (input.value !== "") {
+            input.parentNode.classList.add("is-filled");
+        }
+    });
+
+    // Make phone prefix in phone number field
+    const emailInput = document.querySelector('#email');
+    const emailRegex = /^[a-zA-Z0-9._%+-]{2,25}@[a-zA-Z0-9.-]{1,15}\.[a-zA-Z]{2,6}$/;
+    emailInput.addEventListener('input', function() {
+        const emailValue = this.value.trim();
+
+       if (emailRegex.test(emailValue) || emailValue.length === 0) {
+           this.classList.remove('invalid');
+       } else {
+           this.classList.add('invalid');
+       }
+    });
+
+    const phoneNumberInput = document.querySelector('#phone_number');
+    phoneNumberInput.addEventListener('input', function() {
+        // Remove all non-digit characters except for '+'
+        let cleanedValue = this.value.replace(/[^+\d]/g, '');
+
+        // Add spaces after every 3 characters
+        cleanedValue = cleanedValue.replace(/(.{3})/g, '$1 ');
+
+        // Remove the trailing space (if any)
+        cleanedValue = cleanedValue.trim();
+
+        // Update the input field with the formatted value
+        this.value = cleanedValue;
+
+        // Validate the length of the input (including '+')
+        const inputLength = cleanedValue.replace(/\s/g, '').length;
+
+        // Check if the length is between 9 and 12 characters
+        if ((cleanedValue.startsWith('+48') && inputLength === 12) || (!cleanedValue.startsWith('+48') && inputLength === 9)) {
+            this.classList.remove('invalid');
+        } else {
+            this.classList.add('invalid');
+        }
+    });
 })
