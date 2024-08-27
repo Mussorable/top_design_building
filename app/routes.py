8f07610aeec0fd7faec109ae55b27ed328817d01
Mussorable <redduck5601@gmail.com>
2024-08-27 21:34:41 +0200

    add bottom footer nav-menu, active class for main navigation

--- app/routes.py
+++ app/routes.py
@@ -10,19 +10,19 @@ from flask import render_template, flash, redirect, url_for, request
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title='Home')
+    return render_template('index.html', title='Home', active_page=request.endpoint)
 
 
 @app.route('/about')
 def about():
     folium_map = generate_map()
 
-    return render_template('about_us.html', title='About Us', map=folium_map)
+    return render_template('about_us.html', title='About Us', map=folium_map, active_page=request.endpoint)
 
 
 @app.route('/services')
 def services():
-    return render_template('services.html', title='Services')
+    return render_template('services.html', title='Services', active_page=request.endpoint)
 
 
 @app.route('/contact', methods=['GET', 'POST'])
@@ -31,14 +31,14 @@ def contact():
     if contact_form.validate_on_submit():
         flash('Your message is sent.', 'success')
         return redirect(url_for('contact'))
-    return render_template('contact.html', title='Contact', form=contact_form)
+    return render_template('contact.html', title='Contact', form=contact_form, active_page=request.endpoint)
 
 
 @app.route('/realisations')
 def realisations():
-    return render_template('realisations.html', title='Realisations')
+    return render_template('realisations.html', title='Realisations', active_page=request.endpoint)
 
 
 @app.route('/reviews')
 def reviews():
-    return render_template('reviews.html', title='Reviews')
+    return render_template('reviews.html', title='Reviews', active_page=request.endpoint)
