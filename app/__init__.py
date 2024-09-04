afa91c605d52c8b19d7c0dc4b60bc93a86a2468e
Mussorable <redduck5601@gmail.com>
2024-09-04 21:28:36 +0200

    add libsass, cssmin, started refactoring css files

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,9 @@ app.config.from_object(Config)
 assets = Environment(app)
 
 js_bundle = Bundle('js/main.js', filters='jsmin', output='js/main.min.js')
+css_bundle = Bundle('css/main.scss', filters='libsass', output='css/main.min.css')
 
 assets.register('js_all', js_bundle)
+assets.register('css_all', css_bundle)
 
 from app import routes, forms
--- config.py
+++ config.py
@@ -3,3 +3,4 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    ASSETS_DEBUG = True
