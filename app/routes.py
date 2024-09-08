13e1461fa6b50ef93153ff8236e0a798fe4c477d
Mussorable <redduck5601@gmail.com>
2024-09-08 21:55:42 +0200

    added 404 and 500 error pages

--- app/routes.py
+++ app/routes.py
@@ -77,6 +77,16 @@ def policy():
     return render_template('policy.html', title='Privacy Policy', active_page=request.endpoint)
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
 @app.context_processor
 def utility_processor():
     email_form = EmailForm()
