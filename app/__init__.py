658e8b49ccfe61e421cf7cb8b578561f3fa5c4a9
Mussorable <redduck5601@gmail.com>
2024-09-15 20:39:36 +0200

    commited config changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,7 @@
+import logging
+import os
+from logging.handlers import RotatingFileHandler
+
 from flask import Flask, request
 from flask_assets import Environment, Bundle
 from flask_sqlalchemy import SQLAlchemy
@@ -19,6 +23,22 @@ mail = Mail(app)
 def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
+if not app.debug and not app.testing:
+    if app.config['LOG_TO_STDOUT']:
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.INFO)
+        app.logger.addHandler(stream_handler)
+    else:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/manager.log', maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('App startup')
 
 babel = Babel(app, locale_selector=get_locale)
 
